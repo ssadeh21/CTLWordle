cd942ae6cc3b939e8177f0c5f6d9f2a9c7dbfe58
ssadeh21 <97128078+ssadeh21@users.noreply.github.com>
2022-01-13 18:07:29 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,373 @@
+import { useEffect, useState, useRef } from 'react'
+import { letters, status } from './constants'
+import { Keyboard } from './components/Keyboard'
+import answers from './data/answers'
+import words from './data/words'
+
+import { useLocalStorage } from './hooks/useLocalStorage'
+import { ReactComponent as Info } from './data/Info.svg'
+import { ReactComponent as Settings } from './data/Settings.svg'
+
+import { InfoModal } from './components/InfoModal'
+import { SettingsModal } from './components/SettingsModal'
+import { EndGameModal } from './components/EndGameModal'
+
+const state = {
+  playing: 'playing',
+  won: 'won',
+  lost: 'lost',
+}
+
+const getRandomAnswer = () => {
+  const randomIndex = Math.floor(Math.random() * answers.length)
+  return answers[randomIndex].toUpperCase()
+}
+
+function App() {
+  const initialStates = {
+    answer: () => getRandomAnswer(),
+    gameState: state.playing,
+    board: [
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+    ],
+    cellStatuses: () => Array(6).fill(Array(5).fill(status.unguessed)),
+    currentRow: 0,
+    currentCol: 0,
+    letterStatuses: () => {
+      const letterStatuses = {}
+      letters.forEach((letter) => {
+        letterStatuses[letter] = status.unguessed
+      })
+      return letterStatuses
+    },
+  }
+  const [answer, setAnswer] = useState(initialStates.answer)
+  const [gameState, setGameState] = useState(initialStates.gameState)
+  const [board, setBoard] = useState(initialStates.board)
+  const [cellStatuses, setCellStatuses] = useState(initialStates.cellStatuses)
+  const [currentRow, setCurrentRow] = useState(initialStates.currentRow)
+  const [currentCol, setCurrentCol] = useState(initialStates.currentCol)
+  const [letterStatuses, setLetterStatuses] = useState(initialStates.letterStatuses)
+  const [submittedInvalidWord, setSubmittedInvalidWord] = useState(false)
+  const [currentStreak, setCurrentStreak] = useLocalStorage('current-streak', 0)
+  const [longestStreak, setLongestStreak] = useLocalStorage('longest-streak', 0)
+  const streakUpdated = useRef(false)
+  const [modalIsOpen, setIsOpen] = useState(false)
+  const [firstTime, setFirstTime] = useLocalStorage('first-time', true)
+  const [infoModalIsOpen, setInfoModalIsOpen] = useState(firstTime)
+  const [settingsModalIsOpen, setSettingsModalIsOpen] = useState(false)
+
+  const openModal = () => setIsOpen(true)
+  const closeModal = () => setIsOpen(false)
+  const handleInfoClose = () => {
+    setFirstTime(false)
+    setInfoModalIsOpen(false)
+  }
+
+  const [darkMode, setDarkMode] = useLocalStorage('dark-mode', false)
+  const toggleDarkMode = () => setDarkMode((prev) => !prev)
+
+  useEffect(() => {
+    if (gameState !== state.playing) {
+      setTimeout(() => {
+        openModal()
+      }, 500)
+    }
+  }, [gameState])
+
+  useEffect(() => {
+    if (!streakUpdated.current) {
+      if (gameState === state.won) {
+        if (currentStreak >= longestStreak) {
+          setLongestStreak((prev) => prev + 1)
+        }
+        setCurrentStreak((prev) => prev + 1)
+        streakUpdated.current = true
+      } else if (gameState === state.lost) {
+        setCurrentStreak(0)
+        streakUpdated.current = true
+      }
+    }
+  }, [gameState, currentStreak, longestStreak, setLongestStreak, setCurrentStreak])
+
+  const getCellStyles = (rowNumber, colNumber, letter) => {
+    if (rowNumber === currentRow) {
+      if (letter) {
+        return `nm-inset-background dark:nm-inset-background-dark text-primary dark:text-primary-dark ${
+          submittedInvalidWord ? 'border border-red-800' : ''
+        }`
+      }
+      return 'nm-flat-background dark:nm-flat-background-dark text-primary dark:text-primary-dark'
+    }
+
+    switch (cellStatuses[rowNumber][colNumber]) {
+      case status.green:
+        return 'nm-inset-n-green text-gray-50'
+      case status.yellow:
+        return 'nm-inset-yellow-500 text-gray-50'
+      case status.gray:
+        return 'nm-inset-n-gray text-gray-50'
+      default:
+        return 'nm-flat-background dark:nm-flat-background-dark text-primary dark:text-primary-dark'
+    }
+  }
+
+  const addLetter = (letter) => {
+    setSubmittedInvalidWord(false)
+    setBoard((prev) => {
+      if (currentCol > 4) {
+        return prev
+      }
+      const newBoard = [...prev]
+      newBoard[currentRow][currentCol] = letter
+      return newBoard
+    })
+    if (currentCol < 5) {
+      setCurrentCol((prev) => prev + 1)
+    }
+  }
+
+  const isValidWord = (word) => {
+    if (word.length < 5) return false
+    return words[word.toLowerCase()]
+  }
+
+  const onEnterPress = () => {
+    const word = board[currentRow].join('')
+    if (!isValidWord(word)) {
+      setSubmittedInvalidWord(true)
+      return
+    }
+
+    if (currentRow === 6) return
+
+    updateCellStatuses(word, currentRow)
+    updateLetterStatuses(word)
+    setCurrentRow((prev) => prev + 1)
+    setCurrentCol(0)
+  }
+
+  const onDeletePress = () => {
+    setSubmittedInvalidWord(false)
+    if (currentCol === 0) return
+
+    setBoard((prev) => {
+      const newBoard = [...prev]
+      newBoard[currentRow][currentCol - 1] = ''
+      return newBoard
+    })
+
+    setCurrentCol((prev) => prev - 1)
+  }
+
+  const updateCellStatuses = (word, rowNumber) => {
+    setCellStatuses((prev) => {
+      const newCellStatuses = [...prev]
+      newCellStatuses[rowNumber] = [...prev[rowNumber]]
+      const wordLength = word.length
+      const answerLetters = answer.split('')
+
+      // set all to gray
+      for (let i = 0; i < wordLength; i++) {
+        newCellStatuses[rowNumber][i] = status.gray
+      }
+
+      // check greens
+      for (let i = wordLength - 1; i >= 0; i--) {
+        if (word[i] === answer[i]) {
+          newCellStatuses[rowNumber][i] = status.green
+          answerLetters.splice(i, 1)
+        }
+      }
+
+      // check yellows
+      for (let i = 0; i < wordLength; i++) {
+        if (answerLetters.includes(word[i]) && newCellStatuses[rowNumber][i] !== status.green) {
+          newCellStatuses[rowNumber][i] = status.yellow
+          answerLetters.splice(answerLetters.indexOf(word[i]), 1)
+        }
+      }
+
+      return newCellStatuses
+    })
+  }
+
+  const isRowAllGreen = (row) => {
+    return row.every((cell) => cell === status.green)
+  }
+
+  // every time cellStatuses updates, check if the game is won or lost
+  useEffect(() => {
+    const cellStatusesCopy = [...cellStatuses]
+    const reversedStatuses = cellStatusesCopy.reverse()
+    const lastFilledRow = reversedStatuses.find((r) => {
+      return r[0] !== status.unguessed
+    })
+
+    if (lastFilledRow && isRowAllGreen(lastFilledRow)) {
+      setGameState(state.won)
+    } else if (currentRow === 6) {
+      setGameState(state.lost)
+    }
+  }, [cellStatuses, currentRow])
+
+  const updateLetterStatuses = (word) => {
+    setLetterStatuses((prev) => {
+      const newLetterStatuses = { ...prev }
+      const wordLength = word.length
+      for (let i = 0; i < wordLength; i++) {
+        if (newLetterStatuses[word[i]] === status.green) continue
+
+        if (word[i] === answer[i]) {
+          newLetterStatuses[word[i]] = status.green
+        } else if (answer.includes(word[i])) {
+          newLetterStatuses[word[i]] = status.yellow
+        } else {
+          newLetterStatuses[word[i]] = status.gray
+        }
+      }
+      return newLetterStatuses
+    })
+  }
+
+  const playAgain = () => {
+    setAnswer(initialStates.answer)
+    setGameState(initialStates.gameState)
+    setBoard(initialStates.board)
+    setCellStatuses(initialStates.cellStatuses)
+    setCurrentRow(initialStates.currentRow)
+    setCurrentCol(initialStates.currentCol)
+    setLetterStatuses(initialStates.letterStatuses)
+    closeModal()
+    streakUpdated.current = false
+  }
+
+  const modalStyles = {
+    overlay: {
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: darkMode ? 'hsl(231, 16%, 25%)' : 'hsl(231, 16%, 92%)',
+      zIndex: 99,
+    },
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      transform: 'translate(-50%, -50%)',
+      height: 'calc(100% - 2rem)',
+      width: 'calc(100% - 2rem)',
+      backgroundColor: darkMode ? 'hsl(231, 16%, 25%)' : 'hsl(231, 16%, 92%)',
+      boxShadow: `${
+        darkMode
+          ? '0.2em 0.2em calc(0.2em * 2) #252834, calc(0.2em * -1) calc(0.2em * -1) calc(0.2em * 2) #43475C'
+          : '0.2em 0.2em calc(0.2em * 2) #A3A7BD, calc(0.2em * -1) calc(0.2em * -1) calc(0.2em * 2) #FFFFFF'
+      }`,
+      border: 'none',
+      borderRadius: '1rem',
+      maxWidth: '475px',
+      maxHeight: '650px',
+      position: 'relative',
+    },
+  }
+
+  return (
+    <div className={darkMode ? 'dark' : ''}>
+      <div className={`flex flex-col justify-between h-fill bg-background dark:bg-background-dark`}>
+        <header className="flex items-center py-2 px-3 text-primary dark:text-primary-dark">
+          <button
+            type="button"
+            onClick={() => setSettingsModalIsOpen(true)}
+            className="p-1 rounded-full"
+          >
+            <Settings />
+          </button>
+          <h1 className="flex-1 text-center text-xl xxs:text-2xl sm:text-4xl tracking-wide font-bold font-righteous">
+            WORD MASTER
+          </h1>
+          <button
+            type="button"
+            onClick={() => setInfoModalIsOpen(true)}
+            className="p-1 rounded-full"
+          >
+            <Info />
+          </button>
+        </header>
+        <div className="flex items-center flex-col py-3">
+          <div className="grid grid-cols-5 grid-flow-row gap-4">
+            {board.map((row, rowNumber) =>
+              row.map((letter, colNumber) => (
+                <span
+                  key={colNumber}
+                  className={`${getCellStyles(
+                    rowNumber,
+                    colNumber,
+                    letter
+                  )} inline-flex items-center font-medium justify-center text-lg w-[14vw] h-[14vw] xs:w-14 xs:h-14 sm:w-20 sm:h-20 rounded-full`}
+                >
+                  {letter}
+                </span>
+              ))
+            )}
+          </div>
+        </div>
+        <InfoModal
+          isOpen={infoModalIsOpen}
+          handleClose={handleInfoClose}
+          darkMode={darkMode}
+          styles={modalStyles}
+        />
+        <EndGameModal
+          isOpen={modalIsOpen}
+          handleClose={closeModal}
+          styles={modalStyles}
+          darkMode={darkMode}
+          gameState={gameState}
+          state={state}
+          currentStreak={currentStreak}
+          longestStreak={longestStreak}
+          answer={answer}
+          playAgain={playAgain}
+        />
+        <SettingsModal
+          isOpen={settingsModalIsOpen}
+          handleClose={() => setSettingsModalIsOpen(false)}
+          styles={modalStyles}
+          darkMode={darkMode}
+          toggleDarkMode={toggleDarkMode}
+        />
+        {gameState === state.playing ? (
+          <Keyboard
+            letterStatuses={letterStatuses}
+            addLetter={addLetter}
+            onEnterPress={onEnterPress}
+            onDeletePress={onDeletePress}
+            gameDisabled={gameState !== state.playing}
+          />
+        ) : (
+          <div className="flex-1 flex pt-8 items-start justify-center">
+            <div className={darkMode ? 'dark' : ''}>
+              <button
+                type="button"
+                className="rounded-lg z-10 px-6 py-2 text-lg nm-flat-background dark:nm-flat-background-dark hover:nm-inset-background dark:hover:nm-inset-background-dark text-primary dark:text-primary-dark"
+                onClick={playAgain}
+              >
+                Play Again
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SettingsModal.js
@@ -0,0 +1,103 @@
+import { ReactComponent as Close } from '../data/Close.svg'
+import Modal from 'react-modal'
+import { Switch, RadioGroup } from '@headlessui/react'
+
+Modal.setAppElement('#root')
+
+export const SettingsModal = ({ isOpen, handleClose, styles, darkMode, toggleDarkMode }) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={handleClose}
+      style={styles}
+      contentLabel="Settings Modal"
+    >
+      <div className={`h-full ${darkMode ? 'dark' : ''}`}>
+        <div
+          className={`h-full flex flex-col items-center justify-center max-w-[390px] mx-auto pt-9 text-primary dark:text-primary-dark `}
+        >
+          <h1 className="text-center mb-4 sm:text-3xl text-2xl">Settings</h1>
+          <div className="flex-1 w-full border-b border-slate-400 mb-4">
+            <button
+              className="absolute top-4 right-4 rounded-full nm-flat-background dark:nm-flat-background-dark text-primary dark:text-primary-dark p-1 w-6 h-6 sm:p-2 sm:h-8 sm:w-8 hover:nm-inset-background dark:hover:nm-inset-background-dark"
+              onClick={handleClose}
+            >
+              <Close />
+            </button>
+
+            <Switch.Group as="div" className="flex items-center">
+              <Switch
+                checked={darkMode}
+                onChange={toggleDarkMode}
+                className={`${
+                  darkMode
+                    ? 'nm-inset-yellow-500 border-background-dark'
+                    : 'nm-inset-background border-transparent'
+                } relative inline-flex flex-shrink-0 h-8 w-14 p-1 border-2 rounded-full cursor-pointer transition ease-in-out duration-200`}
+              >
+                <span
+                  aria-hidden="true"
+                  className={`${
+                    darkMode ? 'translate-x-[1.55rem]' : 'translate-x-0'
+                  } absolute pointer-events-none inline-block top-1/2 -translate-y-1/2 h-5 w-5 shadow rounded-full bg-white transform ring-0 transition ease-in-out duration-200`}
+                />
+              </Switch>
+              <Switch.Label as="span" className="ml-3 cursor-pointer">
+                Dark Mode
+              </Switch.Label>
+            </Switch.Group>
+
+            {/*
+            UI switch for selecting difficulty level. Uncomment when difficulty level functionality is implemented
+
+            <RadioGroup value={difficultyLevel} onChange={setDifficultyLevel} className="mt-6">
+              <RadioGroup.Label className="w-full text-center">Difficulty Level</RadioGroup.Label>
+              <div className="grid grid-cols-3 gap-2 rounded-full mt-2 p-1 nm-inset-background dark:nm-inset-background-dark">
+                {Object.keys(difficultyOptions).map((option) => (
+                  <RadioGroup.Option
+                    key={option}
+                    value={option}
+                    className={({ checked }) =>
+                      `text-primary dark:text-primary-dark ${
+                        checked
+                          ? 'bg-white dark:text-primary'
+                          : 'hover:nm-inset-background-sm dark:hover:nm-inset-background-dark-sm'
+                      }
+                        rounded-full py-2 px-3 flex items-center justify-center text-sm font-bold uppercase sm:flex-1 cursor-pointer`
+                    }
+                  >
+                    <RadioGroup.Label as="p">{option}</RadioGroup.Label>
+                  </RadioGroup.Option>
+                ))}
+              </div>
+            </RadioGroup>*/}
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="mb-4">
+              If you're enjoying this game, you can show your support by{' '}
+              <a
+                href="https://www.buymeacoffee.com/katherinecodes"
+                target="_blank"
+                rel="noreferrer"
+              >
+                buying me a coffee
+              </a>
+              <span className="ml-1 text-xs">💛</span>
+            </div>
+            <a
+              href="https://www.buymeacoffee.com/katherinecodes"
+              target="_blank"
+              rel="noreferrer"
+              className="nm-flat-background dark:nm-flat-background-dark rounded-xl hover:nm-inset-background dark:hover:nm-inset-background-dark"
+            >
+              <img
+                alt="buy me a coffee"
+                src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=katherinecodes&button_colour=eab308&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  )
+}
